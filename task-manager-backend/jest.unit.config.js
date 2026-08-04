9bee77eff57ff422e6bc8d8e752732e51b10a546
jonathan <jonatlop@gmail.com>
2026-08-04 15:55:47 -0500

    chore: fix jest.unit.config moduleNameMapper

--- task-manager-backend/jest.unit.config.js
+++ task-manager-backend/jest.unit.config.js
@@ -7,10 +7,9 @@ module.exports = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   moduleNameMapper: {
-    '^@accounting/(.*)$': '<rootDir>/libs/accounting/src/$1',
     '^@api/(.*)$': '<rootDir>/apps/api/src/$1',
-    '^@project/(.*)$': '<rootDir>/apps/project/src/$1',
-    '^@task/(.*)$': '<rootDir>/apps/task/src/$1',
+    '^@project/(.*)$': '<rootDir>/libs/project/src/$1',
+    '^@task/(.*)$': '<rootDir>/libs/task/src/$1',
     '^@shared$': '<rootDir>/libs/shared/src',
     '^@shared/(.*)$': '<rootDir>/libs/shared/src/$1',
   },
